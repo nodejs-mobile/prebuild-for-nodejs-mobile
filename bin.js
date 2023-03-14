470b16a5bd83d2aee23f36e1a7331a7028d0e740
Andre Staltz <andre@staltz.com>
2023-03-14 11:38:08 +0200

    support android targets

--- bin.js
+++ bin.js
@@ -7,9 +7,24 @@ const { chunksToLinesAsync, chomp } = require('@rauschma/stringio');
 const spawn = require('child_process').spawn
 const p = require('util').promisify;
 
+async function echoReadable(readable) {
+  for await (const line of chunksToLinesAsync(readable)) {
+    console.log(chomp(line));
+  }
+}
+
+async function readableToArray(readable) {
+  const arr = [];
+  for await (const line of chunksToLinesAsync(readable)) {
+    arr.push(chomp(line));
+  }
+  return arr;
+}
+
 const VALID_TARGETS = ['ios-arm64', 'ios-x64', 'android-arm', 'android-arm64', 'android-x64'];
 const target = process.argv[2];
 const verbose = process.argv.includes('--verbose');
+const androidSdkVer = 21; // TODO: this should be CLI-configurable
 if (!target) {
   console.error(
     'ERROR: Must specify a target to prebuild-for-nodejs-mobile' +
@@ -27,21 +42,14 @@ if (!VALID_TARGETS.includes(target)) {
   process.exit(1);
 }
 
-async function echoReadable(readable) {
-  for await (const line of chunksToLinesAsync(readable)) {
-    console.log(chomp(line));
-  }
-}
-
-async function readableToArray(readable) {
-  const arr = [];
-  for await (const line of chunksToLinesAsync(readable)) {
-    arr.push(chomp(line));
-  }
-  return arr;
-}
-
 const [platform, arch] = target.split('-');
+
+if (platform === 'android' && !process.env.ANDROID_NDK_HOME) {
+  console.error('ANDROID_NDK_HOME missing. Please call this tool again, ' +
+    'providing the ANDROID_NDK_HOME env var first');
+  process.exit(1);
+}
+
 const cargoBuildTarget =
   target === 'ios-arm64' ? 'aarch64-apple-ios' :
     target === 'ios-x64' ? 'x86_64-apple-ios' :
@@ -51,14 +59,61 @@ const cargoBuildTarget =
             'TODO'
 
 const mobileGyp = require.resolve('nodejs-mobile-gyp/bin/node-gyp.js');
-const nodeMobileHeaders = path.resolve(path.dirname(require.resolve('nodejs-mobile-react-native')), 'ios', 'libnode');
 
 (async function main() {
   const buildOutputFolder = path.join(process.cwd(), 'build', 'Release');
   const prebuildOutputFolder = path.join(process.cwd(), 'prebuilds', target);
   await mkdirp(prebuildOutputFolder);
 
-  let GYP_DEFINES = `OS=${platform} target_arch=${arch} target_platform=${platform}`;
+  const nodeMobileHeaders = path.resolve(
+    path.dirname(require.resolve('nodejs-mobile-react-native')),
+    platform,
+    'libnode'
+  );
+
+  let GYP_DEFINES = `OS=${platform} target_platform=${platform} target_arch=${arch}`;
+
+  const androidEnvs = {};
+  if (platform === 'android') {
+    let compilerPrefix = '';
+    switch (arch) {
+      case 'arm':
+        compilerPrefix = `armv7a-linux-androideabi${androidSdkVer}`
+        break;
+      case 'arm64':
+        compilerPrefix = `aarch64-linux-android${androidSdkVer}`
+        break
+      case 'x64':
+        compilerPrefix = `x86_64-linux-android${androidSdkVer}`
+        break
+    }
+
+    GYP_DEFINES += ` v8_target_arch=${arch}`;
+    GYP_DEFINES += ` android_target_arch=${arch}`;
+    GYP_DEFINES += ` target=${compilerPrefix}`
+    if (process.platform === 'darwin')
+      GYP_DEFINES += ' host_os=mac';
+    else if (process.platform === 'linux')
+      GYP_DEFINES += ' host_os=linux';
+    else {
+      console.error('Compiling Android modules is only supported on Mac or Linux');
+      process.exit(1)
+    }
+
+    let hostTag = '';
+    if (process.platform === 'darwin') {
+      hostTag = 'darwin-x86_64';
+    } else {
+      hostTag = 'linux-x86_64';
+    }
+    const toolchainPath = `${process.env.ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/${hostTag}`
+    androidEnvs.TOOLCHAIN = toolchainPath;
+    androidEnvs.AR = `${toolchainPath}/bin/llvm-ar`;
+    androidEnvs.CC = `${toolchainPath}/bin/${compilerPrefix}-clang`;
+    androidEnvs.CXX = `${toolchainPath}/bin/${compilerPrefix}-clang++`;
+    androidEnvs.LINK = `${toolchainPath}/bin/${compilerPrefix}-clang++`;
+  }
+
   const rebuild = spawn('node', [mobileGyp, 'rebuild', '--napi', '--strip'], {
     cwd: process.cwd(),
     env: {
@@ -69,10 +124,11 @@ const nodeMobileHeaders = path.resolve(path.dirname(require.resolve('nodejs-mobi
       npm_config_platform: platform,
       npm_config_format: platform === 'ios' ? 'make-ios' : 'make-android',
       npm_config_arch: arch,
+      ...androidEnvs,
     }
   })
 
-  let stderr;
+  let stderr = [];
   if (verbose) {
     await Promise.all([
       echoReadable(rebuild.stdout),
