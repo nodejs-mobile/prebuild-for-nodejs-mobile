5b66d6798b7acefb4bdb9b8356448d9b9707f44f
Andre Staltz <andre@staltz.com>
2023-10-16 17:22:37 +0300

    add ios-arm64 gyp test

--- test/gyp.test.js
+++ test/gyp.test.js
@@ -5,6 +5,41 @@ const path = require('path');
 const rimraf = require('rimraf');
 const fs = require('fs');
 
+tape('GYP-based native module for ios-arm64', async (t) => {
+  if (process.platform !== 'darwin') {
+    t.skip('Can only run on macOS');
+    t.end();
+    return;
+  }
+  const prebuild4 = path.join(__dirname, '..', 'bin.js');
+  const project = path.join(__dirname, 'helloworld');
+  const target = 'ios-arm64';
+
+  if (!fs.existsSync(path.join(project, 'node_modules'))) {
+    await p(exec)('npm install', {cwd: project});
+    t.pass('npm install');
+    await rimraf(path.join(project, 'build'));
+  }
+
+  const task1 = await p(exec)(`${prebuild4} ${target}`, {cwd: project});
+  t.true(task1.stdout.includes('BUILT '), 'prebuild done');
+
+  const pathToOutput = path.join(
+    'prebuilds',
+    target,
+    'helloworld.node',
+    'helloworld',
+  );
+
+  const task2 = await p(exec)(`file ${pathToOutput}`, {cwd: project});
+  t.true(
+    task2.stdout.includes(
+      'Mach-O 64-bit dynamically linked shared library arm64',
+    ),
+    'Mach-O correct',
+  );
+});
+
 tape('GYP-based native module for android-arm(v7)', async (t) => {
   const prebuild4 = path.join(__dirname, '..', 'bin.js');
   const project = path.join(__dirname, 'helloworld');
